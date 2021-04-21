c4116f21d9c33ed80ee58cf9c1843fb3430e8cf6
Lasha Rekhviashvili <lasha.rekh@gmail.com>
2021-04-21 21:06:25 +0400

    forms

--- G12_Store/Store.App/Program.cs
+++ G12_Store/Store.App/Program.cs
@@ -1,5 +1,3 @@
-using Store.Domain;
-using Store.Repository;
 using System;
 using System.Windows.Forms;
 
@@ -16,38 +14,13 @@ namespace Store.App
             Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new MainForm());
 
-
-            //var userRep = new UserRepository();
-            //var user1 = new User() { ID = 2, Username = "Malgusss", Password = "123", PermissionGroupID = 1 };
-            //userRep.Add(user1);
-            //userRep.Edit(user1);
-            //userRep.Delete(user1);
-            //var userID = userRep.Login(user1.Username, user1.Password, out string message);
-
-            //var psREP = new ProductSuplierRepository();
-            //var ps = new ProductSuplier()
-            //{
-            //    BankName = "afiorabank",
-            //    Description = "Desc",
-            //    Email = "email@email.com",
-            //    IBAN = "123123",
-            //    ID = 1,
-            //    LegalAddress = "address",
-            //    Phone = "123123",
-            //    Phone2 = "123123213",
-            //    PostalCode = "123123",
-            //    SuplierName = "Wukong",
-            //    TransportationAddress = "asd123",
-            //    VatID = "123123123",
-            //    IsDeleted = false,
-            //    SuplierGroupID = 1,
-            //    asdas 
-
-            //};
-            //psREP.Add(ps);
+            var loginForm = new LoginForm();
+            var result = loginForm.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                Application.Run(new MainForm());
+            }
         }
-
     }
 }
