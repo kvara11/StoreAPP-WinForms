9f3f71bf06c49159c77478ff37fac7d36d858cae
Lasha Rekhviashvili <lasha.rekh@gmail.com>
2021-05-05 21:51:14 +0400

    PermissionRender

--- G12_Store/Store.App/MainForm.cs
+++ G12_Store/Store.App/MainForm.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Store.Repository;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -16,7 +17,35 @@ namespace Store.App
         {
             InitializeComponent();
             this.Text += $" ({LocalStorage.UserFullName})";
+
+            //salesBtn.Tag = "0,1,9";
+            RenderByPermissions();
         }
 
+        private void RenderByPermissions()
+        {
+            short[] GetControlPermissions(Control control)
+            {
+                string[] permissionsStr = control.Tag.ToString().Split(',');
+                short[] permissions = new short[permissionsStr.Length];
+                for (int i = 0; i < permissionsStr.Length; i++)
+                {
+                    permissions[i] = short.Parse(permissionsStr[i]);
+                }
+                return permissions;
+            }
+
+            foreach (Control control in this.Controls)
+            {
+                if (control.Tag == null)
+                    continue;
+                short[] controlPermissions = GetControlPermissions(control);
+
+                if (LocalStorage.PermissionCodes.Intersect(controlPermissions).Count() == 0)
+                {
+                    control.Visible = false;
+                }
+            }
+        }
     }
 }
