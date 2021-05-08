3f20a1eddb94504475fc624df742466a4665557a
Lasha Rekhviashvili <lasha.rekh@gmail.com>
2021-05-08 20:59:23 +0400

    extend controlls

--- G12_Store/Store.App/MainForm.cs
+++ G12_Store/Store.App/MainForm.cs
@@ -1,13 +1,4 @@
-﻿using Store.Repository;
-using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
+﻿using System.Windows.Forms;
 
 namespace Store.App
 {
@@ -19,33 +10,7 @@ namespace Store.App
             this.Text += $" ({LocalStorage.UserFullName})";
 
             //salesBtn.Tag = "0,1,9";
-            RenderByPermissions();
-        }
-
-        private void RenderByPermissions()
-        {
-            short[] GetControlPermissions(Control control)
-            {
-                string[] permissionsStr = control.Tag.ToString().Split(',');
-                short[] permissions = new short[permissionsStr.Length];
-                for (int i = 0; i < permissionsStr.Length; i++)
-                {
-                    permissions[i] = short.Parse(permissionsStr[i]);
-                }
-                return permissions;
-            }
-
-            foreach (Control control in this.Controls)
-            {
-                if (control.Tag == null)
-                    continue;
-                short[] controlPermissions = GetControlPermissions(control);
-
-                if (LocalStorage.PermissionCodes.Intersect(controlPermissions).Count() == 0)
-                {
-                    control.Visible = false;
-                }
-            }
+            Controls.RenderByPermissions();
         }
     }
 }
---
+++ G12_Store/Store.App/PermissionHelper.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Store.App
+{
+    static class PermissionHelper
+    {
+        public static void RenderByPermissions(this Control.ControlCollection controls)
+        {
+            short[] GetControlPermissions(Control control)
+            {
+                string[] permissionsStr = control.Tag.ToString().Split(',');
+                short[] permissions = new short[permissionsStr.Length];
+                for (int i = 0; i < permissionsStr.Length; i++)
+                {
+                    permissions[i] = short.Parse(permissionsStr[i]);
+                }
+                return permissions;
+            }
+
+            foreach (Control control in controls)
+            {
+                if (string.IsNullOrEmpty(control.Tag?.ToString()))
+                    continue;
+                short[] controlPermissions = GetControlPermissions(control);
+
+                if (LocalStorage.PermissionCodes.Intersect(controlPermissions).Count() == 0)
+                {
+                    control.Visible = false;
+                }
+            }
+        }
+    }
+}
